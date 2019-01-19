b96349e4ced5dcd531ba085b9a7f98d6e874da59
Jared Smith <jared@regen.network>
2019-01-19 02:36:45 -0700

    new colors

--- mudcolors.js
+++ mudcolors.js
@@ -37,15 +37,15 @@ mudcolors.colors = [
 	{code: '\u001b[34m', tag: 'u', color: 'blue'}, //blue
 	{code: '\u001b[35m', tag: 'm', color: 'magenta'}, //magenta
 	{code: '\u001b[36m', tag: 'c', color: 'cyan'}, //cyan
-	{code: '\u001b[0m', tag: 'w', color: 'white'}, //white
-	{code: '\u001b[30;1m', tag: 'B', color: 'BLACK'}, //BLACK
-	{code: '\u001b[31;1m', tag: 'R', color: 'RED'}, //RED
-	{code: '\u001b[32;1m', tag: 'G', color: 'GREEN'}, //GREEN
-	{code: '\u001b[33;1m', tag: 'Y', color: 'YELLOW'}, //YELLOW
-	{code: '\u001b[34;1m', tag: 'U', color: 'BLUE'}, //BLUE
-	{code: '\u001b[35;1m', tag: 'M', color: 'MAGENTA'}, //MAGENTA
-	{code: '\u001b[36;1m', tag: 'C', color: 'CYAN'}, //CYAN
-	{code: '\u001b[0;1m', tag: 'W', color: 'WHITE'} //WHITE
+	{code: '\u001b[37m', tag: 'w', color: 'white'}, //white
+	{code: '\u001b[90m', tag: 'B', color: 'BLACK'}, //BLACK
+	{code: '\u001b[91m', tag: 'R', color: 'RED'}, //RED
+	{code: '\u001b[92m', tag: 'G', color: 'GREEN'}, //GREEN
+	{code: '\u001b[93m', tag: 'Y', color: 'YELLOW'}, //YELLOW
+	{code: '\u001b[94m', tag: 'U', color: 'BLUE'}, //BLUE
+	{code: '\u001b[95m', tag: 'M', color: 'MAGENTA'}, //MAGENTA
+	{code: '\u001b[96m', tag: 'C', color: 'CYAN'}, //CYAN
+	{code: '\u001b[97m', tag: 'W', color: 'WHITE'} //WHITE
 ];
 
 module.exports = mudcolors;
